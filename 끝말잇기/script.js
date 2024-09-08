eef2fb470ba62b7993197fc072889f6f19db20fd
choseongchan3712 <choseongchan3712@naver.com>
2024-09-09 00:16:57 +0900

    참가자수증가해결

--- 끝말잇기/script.js
+++ 끝말잇기/script.js
@@ -8,4 +8,18 @@ const order = document.querySelector('#order');
 const word = document.querySelector('#word');
 const input = document.querySelector('#input');
 const button = document.querySelector('#button');
+const regex = /^[ㄱ-ㅎ|가-힣]+$/;
 
+var num = 1;
+order.textContent = num; //userNum 함수 뿐만 아니라 여기에도 order.textContent를 써야 처음에 들어갔을때 1이 나옴
+
+const userNum = () => {
+  if (num < number) {
+    num++;
+  } else {
+    num = 1;
+  }
+  order.textContent = num;
+};
+
+button.addEventListener("click", userNum);
