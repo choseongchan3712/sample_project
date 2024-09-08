3051490f97c9fe0c1569119a165e7427b6ecf789
choseongchan3712 <choseongchan3712@naver.com>
2024-09-09 01:07:40 +0900

    초기완성

--- 끝말잇기/script.js
+++ 끝말잇기/script.js
@@ -9,6 +9,7 @@ const word = document.querySelector('#word');
 const input = document.querySelector('#input');
 const button = document.querySelector('#button');
 const regex = /^[ㄱ-ㅎ|가-힣]+$/;
+let words = [];
 
 var num = 1;
 order.textContent = num; //userNum 함수 뿐만 아니라 여기에도 order.textContent를 써야 처음에 들어갔을때 1이 나옴
@@ -23,3 +24,34 @@ const userNum = () => {
 };
 
 button.addEventListener("click", userNum);
+
+const game = () => {
+  let orderValue = Number(num); //n번째 참가자의 n
+  let inputWord = document.getElementById("input").value; //input창에 입력되는 단어
+  let i = 1;
+
+  if (regex.test(inputWord) == false) {
+    alert(`단어를 다시 입력하세요 ${i++} / 3`);
+    input.value = "";
+    if (i > 3) {
+      alert("값을 잘못 입력하셨습니다. 게임이 끝났습니다.")
+    }
+  } else {
+    let wordsLength = words.length;
+    if (wordsLength === 0) { //아무런 단어도 아직 받지 않았을 때
+      word.append(inputWord + "-"); //? append() -> 컨텐츠를 선택된 요소 내부의 끝 부분에서 삽입
+      words.push(inputWord); //? 배열에 단어 추가
+    } else { //이미 단어를 받은 후라면
+      let preWord = words[words.length - 1];
+      if (preWord.substr(-1) == inputWord.substr(0, 1)) {
+        word.append(inputWord + "-");
+        words.push(inputWord);
+      } else {
+        alert("단어를 잘못 입력하셨습니다. 게임이 끝났습니다")
+      }
+    }
+    input.value = ""; //input창 초기화
+  }
+};
+
+button.addEventListener("click", game);
