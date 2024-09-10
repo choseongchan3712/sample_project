898e99744f0c096b990599b7078833b0bbd82813
choseongchan3712 <choseongchan3712@naver.com>
2024-09-11 05:39:22 +0900

    계산기 2차 완성

--- 계산기/script.js
+++ 계산기/script.js
@@ -1,2 +1,25 @@
-const calculateResult = document.querySelector('.result');
+const calculateResult = document.querySelector('input');
+const num0 = document.querySelector('.num_0');
+const num1 = document.querySelector('.num_1');
+const num2 = document.querySelector('.num_2');
+const num3 = document.querySelector('.num_3');
+const num4 = document.querySelector('.num_4');
+const num5 = document.querySelector('.num_5');
+const num6 = document.querySelector('.num_6');
+const num7 = document.querySelector('.num_7');
+const num8 = document.querySelector('.num_8');
+const num9 = document.querySelector('.num_9');
+const plus = document.querySelector('.plus');
+const minus = document.querySelector('.minus');
+const divide = document.querySelector('.divide');
+const multiply = document.querySelector('.multiply');
+const clear = document.querySelector('.clear');
+const calculate = document.querySelector('.calculate');
 
+let numOne;
+let numTwo;
+let operator;
+
+// const click = (e) => {
+//   if (e.target.textContent == "0" || e.target.textContent == "1" || e.target.textContent == "2" || e.target.textContent == "3" || e.target.textContent == "4" || e.target.textContent == "5" || e.target.textContent == "6" || e.target.textContent == "7" ||)
+// }
