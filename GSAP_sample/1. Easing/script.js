cfae368ef33ba6bbecb142067d91177fafbbb71f
choseongchan3712 <choseongchan3712@naver.com>
2024-10-06 21:09:18 +0900

    easing

---
+++ GSAP_sample/1. Easing/script.js
@@ -0,0 +1,87 @@
+gsap.to(".bounce_in", { 
+  rotation: 360,
+  duration: 2,
+  repeat: -1,
+  repeatDelay: 2,
+  ease: 'bounce.in'
+});
+gsap.to(".bounce_out", { 
+  rotation: 360,
+  duration: 2,
+  repeat: -1,
+  repeatDelay: 2,
+  ease: 'bounce.out'
+});
+gsap.to(".bounce_in_out", { 
+  rotation: 360,
+  duration: 2,
+  repeat: -1,
+  repeatDelay: 2,
+  ease: 'bounce.inOut'
+});
+
+
+gsap.to(".power1_in", { 
+  rotation: 360,
+  duration: 2,
+  repeat: -1,
+  repeatDelay: 2,
+  ease: 'power1.in'
+});
+gsap.to(".power1_out", { 
+  rotation: 360,
+  duration: 2,
+  repeat: -1,
+  repeatDelay: 2,
+  ease: 'power1.out'
+});
+gsap.to(".power1_in_out", { 
+  rotation: 360,
+  duration: 2,
+  repeat: -1,
+  repeatDelay: 2,
+  ease: 'power1.inOut'
+});
+
+
+gsap.to(".elastic_in", { 
+  rotation: 360,
+  duration: 2,
+  repeat: -1,
+  repeatDelay: 2,
+  ease: "elastic.in(1,0.3)"
+});
+gsap.to(".elastic_out", { 
+  rotation: 360,
+  duration: 2,
+  repeat: -1,
+  repeatDelay: 2,
+  ease: "elastic.out(1,0.3)"
+});
+gsap.to(".elastic_in_out", { 
+  rotation: 360,
+  duration: 2,
+  repeat: -1,
+  repeatDelay: 2,
+  ease: "elastic.inOut(1,0.3)"
+});
+
+
+gsap.to(".steps", { 
+  rotation: 360,
+  duration: 2,
+  repeat: -1,
+  repeatDelay: 2,
+  ease: "steps(12)"
+});
+
+
+
+
+
+
+
+
+
+
+
